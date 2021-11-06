dcd6e283fcf2af1efed5de751de024ca54071429
Mohammad Yousuf Minhaj Zia <yousuf@glints.com>
2021-11-06 20:38:02 +0530

    cache: added Cache.hpp and Cache.cpp

---
+++ src/Cache.cpp
@@ -0,0 +1,80 @@
+#include "Cache.hpp"
+
+#include <cassert>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+namespace CacheSim {
+void Cache::lruShuffle(uint32_t setNum, uint32_t indexInSet) {
+  vector<CacheLine>& currentSet = store[setNum];
+  CacheLine line = currentSet[indexInSet];
+  currentSet.erase(currentSet.begin() + indexInSet);
+  currentSet.push_back(line);
+}
+
+CacheLine& Cache::get(uint32_t addr) {
+  assert(has(addr));
+  int blockNumber = addr / blockSize;
+  int setNum = blockNumber % totalSets;
+  int indexInSet = getIndexInSet(addr);
+  lruShuffle(setNum, indexInSet);
+  return store[setNum][indexInSet];
+}
+
+int Cache::getInvalidLineFromSet(uint32_t setNum) {
+  std::vector<CacheLine>& currentSet = store[setNum];
+  for (int num = 0; num < associativity; num++) {
+    CacheLine line = currentSet[num];
+    if (line.state == CacheLine::CacheState::INVALID) {
+      return num;
+    }
+  }
+  return -1;
+}
+
+int Cache::getEvictionCandidateFromSet(uint32_t setNum) {
+  int invalidIndex = getInvalidLineFromSet(setNum);
+  if (invalidIndex == -1) {
+    return invalidIndex;
+  }
+  return store[setNum].size() - 1;
+}
+
+void Cache::evict(uint32_t addr) {
+  assert(!has(addr));
+  int blockNumber = addr / blockSize;
+  int setNum = blockNumber % totalSets;
+  int lineToEvect = getEvictionCandidateFromSet(setNum);
+  store[setNum][lineToEvect] = CacheLine();
+}
+
+int Cache::getIndexInSet(uint32_t addr) {
+  int blockNumber = addr / blockSize;
+  int setNum = blockNumber % totalSets;
+  std::vector<CacheLine>& currentSet = store[setNum];
+
+  for (int num = 0; num < associativity; num++) {
+    CacheLine line = currentSet[num];
+    if (line.state != CacheLine::CacheState::INVALID && line.blockNumber == blockNumber) {
+      return num;
+    }
+  }
+
+  return -1;
+}
+
+bool Cache::has(uint32_t addr) {
+  return getIndexInSet(addr) != -1;
+}
+
+void Cache::put(uint32_t addr, CacheLine::CacheState state, int validFrom) {
+  assert(!has(addr));
+  int blockNumber = addr / blockSize;
+  int setNum = blockNumber % totalSets;
+  int lineToEvect = getEvictionCandidateFromSet(setNum);
+  store[setNum][lineToEvect] = CacheLine(state, blockNumber);
+}
+}// namespace CacheSim
---
+++ src/Cache.hpp
@@ -0,0 +1,56 @@
+#ifndef CS4223_CACHE_SIM_PROCESSOR_HPP
+#define CS4223_CACHE_SIM_PROCESSOR_HPP
+
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+namespace CacheSim {
+class CacheLine {
+ public:
+  enum CacheState {
+    INVALID,
+    MODIFIED
+  };
+
+  // INVALID, DIRTY
+  CacheState state;
+  int blockNumber;
+  int validFrom;
+  CacheLine(CacheState state = INVALID, int blockNumber = -1, int validFrom = -1) : state(state),
+                                                                                    blockNumber(blockNumber),
+                                                                                    validFrom(validFrom){};
+};
+
+class Cache {
+ private:
+  void lruShuffle(uint32_t setNum, uint32_t indexInSet);
+  int getIndexInSet(uint32_t addr);
+  int getInvalidLineFromSet(uint32_t setNum);
+  int getEvictionCandidateFromSet(uint32_t setNum);
+
+ public:
+  uint16_t associativity;
+  uint16_t totalSets;
+  uint16_t blockSize;
+  std::vector<std::vector<CacheLine>> store;
+
+  // 4KB 32-byte 2-way set associative -> totalSets = 4 * 1024 / 2 / 32 = 64 blocks
+  Cache(uint16_t associativity = 2, uint16_t totalSets = 64,
+        uint32_t blockSize = 32) : associativity(associativity),
+                                   totalSets(totalSets),
+                                   blockSize(blockSize) {
+    this->store = std::vector<std::vector<CacheLine>>(totalSets,
+                                                      std::vector<CacheLine>(associativity, CacheLine()));
+  }
+
+  bool has(uint32_t addr);
+  CacheLine& get(uint32_t addr);
+  void put(uint32_t addr, CacheLine::CacheState state, int validFrom);
+  void evict(uint32_t addr);
+};
+}// namespace CacheSim
+
+#endif//CS4223_CACHE_SIM_PROCESSOR_HPP
