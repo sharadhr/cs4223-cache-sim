218261674e02c4c3129b9d2d05e21fd9a0791b2e
Sharadh Rajaraman <r.sharadh@yahoo.com.sg>
2021-11-12 19:41:57 +0800

    feat: Moved out Instruction struct

---
+++ src/Instruction.hpp
@@ -0,0 +1,18 @@
+#ifndef CS4223_CACHE_SIM_INSTRUCTION_HPP
+#define CS4223_CACHE_SIM_INSTRUCTION_HPP
+
+namespace CacheSim {
+struct Instruction {
+  enum class InstructionType { LD, ST, ALU, DONE };
+  InstructionType type{InstructionType::ALU};
+  uint32_t value{};
+
+  Instruction() : type(InstructionType::ALU), value(0){};
+  Instruction(uint8_t typeNum, uint32_t value) : type(InstructionType(typeNum)), value(value){};
+  Instruction(InstructionType type, uint32_t value) : type(type), value(value){};
+};
+
+using Type = Instruction::InstructionType;
+}// namespace CacheSim
+
+#endif//CS4223_CACHE_SIM_INSTRUCTION_HPP
